356e39aba8065c0dda244e62d74afd8902acccca
Ramil Algayev <game.rcgoffcial@gmail.com>
2022-07-16 19:57:26 +0400

    testing again and again

--- testing_ttt.c
+++ testing_ttt.c
@@ -3,8 +3,26 @@
 
 int main(int argc, char const *argv[])
 {
-    printf("Hello, world!\n");
+  //fibbionachi series
+  long a = 1;
+  long b = 1;
+  long limit = 1;
+
+  do
+  {
+    printf("List fibbionachi till nth element(input n): ");
+    scanf("%ld", &limit);
+  }while(limit < 0);
+
+  for(long i = 0; i <= limit; ++i)
+  {
+    printf("%ld\n", a);
+    a += b;
+    a ^= b;
+    b ^= a;
+    a ^= b;
+  }
 
 
-    return 0;
+  return 0;
 }
